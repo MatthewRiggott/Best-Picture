24c280cbb3b50cece20a52faf06e892e91f2df9c
Matthew Riggott <Matthew.S.Riggott@gmail.com>
2015-06-02 17:31:17 -0400

    lets all pray that this actualy works because I can't test it

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,8 +1,18 @@
 class Vote < ActiveRecord::Base
+  after_create :check_votes
+
   belongs_to :user
   belongs_to :contest
   belongs_to :photo
 
   validates :user, uniqueness: { scope: :contest,
     message: "can only vote once per contest" }
+
+  private
+
+  def check_votes
+    if !user.can_view
+      user.can_view = true if user.votes.count >= 2
+    end
+  end
 end
